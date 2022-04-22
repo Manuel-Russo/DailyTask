1f17fa0b1494c0d2b95374400d32245c43a94ad9
Russino <manuelrusso070@gmail.com>
2022-04-22 22:03:32 +0200

    mainApp

---
+++ DailyTask/app/src/main/java/com/example/dailytask/Evento.java
@@ -0,0 +1,4 @@
+package com.example.dailytask;
+
+public class Evento {
+}
---
+++ DailyTask/app/src/main/java/com/example/dailytask/OrarioScuolaActivity.java
@@ -0,0 +1,14 @@
+package com.example.dailytask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrarioScuolaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orario_scuola);
+    }
+}

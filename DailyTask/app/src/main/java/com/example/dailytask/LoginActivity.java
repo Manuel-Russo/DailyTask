dfb9c1ff7190c4b3bb3b2c4de9a7e00e11d5480f
Russino <manuelrusso070@gmail.com>
2022-04-26 17:50:23 +0200

    V 0.0.1
    aggiunta Login

---
+++ DailyTask/app/src/main/java/com/example/dailytask/LoginActivity.java
@@ -0,0 +1,71 @@
+package com.example.dailytask;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            this::onSignInResult
+    );
+    List<AuthUI.IdpConfig> providers = Collections.singletonList(
+            new AuthUI.IdpConfig.EmailBuilder().build());
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getSupportActionBar()!=null)    {
+            Objects.requireNonNull(getSupportActionBar()).hide(); //nasconde barra
+        }
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null){
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+
+        setContentView(R.layout.activity_login);
+
+        Button button= findViewById(R.id.sign_in_button);
+        button.setOnClickListener(view -> {
+            List<AuthUI.IdpConfig> providers = Collections.singletonList(
+                    new AuthUI.IdpConfig.EmailBuilder().setAllowNewAccounts(false).build());
+
+            Intent signInIntent = AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .setIsSmartLockEnabled(false, false)
+                    .build();
+            signInLauncher.launch(signInIntent);
+        });
+
+    }
+
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            Intent intent=new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "Login non effettuato", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
